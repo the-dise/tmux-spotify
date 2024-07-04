4ae03a4fc3b77f10d8aa700a9abc58836337f369
ダイス <31319017+the-dise@users.noreply.github.com>
2024-07-04 16:07:57 +0300

    Add files via upload

---
+++ spotify.sh
@@ -0,0 +1,16 @@
+# If this module depends on an external Tmux plugin, say so in a comment.
+# E.g.: Requires https://github.com/aaronpowell/tmux-weather
+
+show_spotify() { # This function name must match the module name!
+  local index icon color text module
+
+  index=$1 # This variable is used internally by the module loader in order to know the position of this module
+
+  icon="$(  get_tmux_option "@catppuccin_spotify_icon"  "󰓇" )"
+  color="$( get_tmux_option "@catppuccin_spotify_color" "$thm_green" )"
+  text="$(  get_tmux_option "@catppuccin_spotify_text"  "#{spotify_status}" )"
+
+  module=$( build_status_module "$index" "$icon" "$color" "$text" )
+
+  echo "$module"
+}
